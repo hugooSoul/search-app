c78731125a1fa97c7df970c02fa19033581102bf
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-05 21:08:56 -0600

    Search app using create react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Grid, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <Grid>
+            <h1>Search App</h1>
+            <p>This is a simple search app</p>
+
+            <SearchForm />
+          </Grid>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
